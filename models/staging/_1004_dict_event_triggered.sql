2ee018a36f6b28421e80990a7fab09ec264555ba
PALASH SAXENA <palash@askphill.com>
2023-07-18 14:24:02 +0000

    changed trigger values

--- models/staging/_1004_dict_event_triggered.sql
+++ models/staging/_1004_dict_event_triggered.sql
@@ -3,12 +3,12 @@ with _0001 as (select * from {{ref('_1001_events_transform')}})
 select distinct
     user_pseudo_id,
     session_id,
-    max(case when lower(event_name) = 'add_to_cart' then 1 else 0 end) as addtocart_triggered,
-    max(case when lower(event_name) = 'purchase' then 1 else 0 end) as purchase_triggered,
-    max(case when lower(event_name) = 'view_item' then 1 else 0 end) as viewitem_triggered,
-    max(case when lower(event_name) = 'view_cart' then 1 else 0 end) as viewcart_triggered,
-    max(case when lower(event_name) = 'begin_checkout' then 1 else 0 end) as checkout_triggered,
-    max(case when lower(event_name) = 'view_item' then 1 else 0 end) as PDP_seen,
-    max(case when lower(event_name) = 'view_item_list' then 1 else 0 end) as collection_page_seen,    
+    max(case when lower(event_name) = 'add_to_cart' then 'add_to_cart' else null end) as addtocart_triggered,
+    max(case when lower(event_name) = 'purchase' then 'purchase' else null end) as purchase_triggered,
+    max(case when lower(event_name) = 'view_item' then 'view_item' else null end) as viewitem_triggered,
+    max(case when lower(event_name) = 'view_cart' then 'view_cart' else null end) as viewcart_triggered,
+    max(case when lower(event_name) = 'begin_checkout' then 'begin_checkout' else null end) as checkout_triggered,
+    max(case when lower(event_name) = 'view_item' then 'PDP' else null end) as PDP_seen,
+    max(case when lower(event_name) = 'view_item_list' then 'PLP' else null end) as collection_page_seen,    
 from _0001
 group by user_pseudo_id, session_id
