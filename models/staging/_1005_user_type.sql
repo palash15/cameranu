079ac81498f4887a60e656d89281994a5519edbe
PALASH SAXENA <palash@askphill.com>
2023-07-19 12:58:38 +0000

    added user type

---
+++ models/staging/_1005_user_type.sql
@@ -0,0 +1,12 @@
+with _0001 as (select * from {{ref('_1001_events_transform')}})
+
+select distinct
+    user_pseudo_id,
+    event_date,
+    session_id,
+    session_count,
+    case when session_count = 1 then 'new_user'
+         when session_count > 1 then 'returning_user'
+    end as user_type
+from _0001
+order by user_pseudo_id
