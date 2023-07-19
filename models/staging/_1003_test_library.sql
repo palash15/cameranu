e80bf5ffe8a4bc10fc29b58fbea9f6633fdf063f
PALASH SAXENA <palash@askphill.com>
2023-07-19 13:36:26 +0000

    added standard variant types

--- models/staging/_1003_test_library.sql
+++ models/staging/_1003_test_library.sql
@@ -7,7 +7,11 @@ select distinct
     vwo_campaign_id as experiment_id,
     vwo_campaign_name as experiment_name,
     vwo_variant_id as variant_id,
-    vwo_variant_name as variant_name
+    vwo_variant_name as variant_name,
+    case when vwo_variant_name is not null then 'Variant' 
+         when lower(vwo_variant_name) like '%control%' then 'Control'
+    end as variant_type
+         
 from _0001
 where lower(event_name) like '%vwo%' and vwo_campaign_name is not null and vwo_variant_name is not null
 order by user_pseudo_id
