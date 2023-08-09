889ce65e8b85c2056f4775c2087f400895f4a96f
PALASH SAXENA <palash@askphill.com>
2023-08-09 09:45:42 +0000

    refined fact table

--- models/final/_9005_fact_table.sql
+++ models/final/_9005_fact_table.sql
@@ -1,26 +1,14 @@
 with _1000 as (
     select * from {{ref('_1000_users')}}
     where experiment_name is not null and event_category != 'other events'
-),
-
-dates as (
-    select experiment_name as experiments, min(event_date) as min_date, max(event_date) as max_date 
-    from _1000 
-    group by experiment_name
-),
-
-metrics as (
-    select
-        experiment_name as experiments, 
-        variant_name as variants,
-        device,
-        event_name
-    from _1000
-    group by experiment_name, variant_name, device, event_name
 )
 
 select
-    dates.min_date,
-    dates.max_date,
-    metrics.*
-from dates left join metrics on dates.experiments = metrics.experiments
+    experiment_name as experiments, 
+    min(event_date) as min_date, 
+    max(event_date) as max_date,
+    array_agg(distinct variant_name) as variants,
+    array_agg(distinct device) as devices,
+    array_agg(distinct event_name) as event_names
+from _1000
+group by experiment_name
