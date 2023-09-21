d9f253df746fae67aeac6bdf2a35b2fffa131fd0
BigQuery Primera <analytics@datablaze.ai>
2023-09-21 15:48:41 +0000

    user role

--- models/final/_9005_fact_table.sql
+++ models/final/_9005_fact_table.sql
@@ -10,6 +10,6 @@ select
     string_agg(distinct variant_name, ',') as variants,
     string_agg(distinct device, ',') as devices,
     string_agg(distinct event_name, ',') as event_names,
-    'primera@valantic.nl' as user_key
+    'primera@valantic.nl' as user_role
 from _1000
 group by experiment_name
