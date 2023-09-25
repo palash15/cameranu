4a6e9aad450914c5618a2dea1370760817371a55
BigQuery Primera <analytics@datablaze.ai>
2023-09-25 12:56:31 +0000

    9001 REVERT

--- models/final/_9001_device_data.sql
+++ models/final/_9001_device_data.sql
@@ -1,10 +1,3 @@
-with _1000 as (
-    SELECT distinct event_date, experiment_name, device, variant_name, user_type,
-                    channel_grouping_session, browser,
-                    string_agg(distinct user_pseudo_id) as user_pseudo_ids,
-                    count(distinct transaction_id) as transaction_id,
-                    sum(revenue) as revenue
-  FROM {{ref('_1000_users')}}
-  GROUP BY EVENT_DATE, EXPERIMENT_NAME, VARIANT_NAME, DEVICE, USER_TYPE, CHANNEL_GROUPING_SESSION, BROWSER)
+with _1000 as (select * from {{ref('_1000_users')}})
 
-SELECT * FROM _1000
+select * from _1000
