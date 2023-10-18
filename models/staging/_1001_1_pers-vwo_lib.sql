1776931690001521eafda9ebaf20d460fe68a97e
primera dbt <bigqueryprimera@gmail.com>
2023-10-18 10:55:18 +0000

    fixed data pipeline

--- models/staging/_1001_1_pers-vwo_lib.sql
+++ models/staging/_1001_1_pers-vwo_lib.sql
@@ -6,48 +6,19 @@ clean_events as (
     user_pseudo_id AS user_pseudo_id_1,
     (select value.int_value from unnest(event_params) where key = "ga_session_id") as session_id_1,
     event_name,
-    (select value.string_value from unnest(event_params) where key = "exp_variant_string") as experiment_information,
-    (select value.int_value from unnest(event_params) where key = "personalization_id") as personalization_id,
-    (select value.string_value from unnest(event_params) where key = "personalization_name") as personalization_name,
-    (select value.int_value from unnest(event_params) where key = "personalization_variant_id") as personalization_variant_id,
-    (select value.string_value from unnest(event_params) where key = "variant_name") as personalization_variant_name,
+    (select value.string_value from unnest(event_params) where key = "vwo_campaign_name") as experiment_id,
+    (select value.string_value from unnest(event_params) where key = "vwo_variation_name") as variant_id,
+    (select value.string_value from unnest(event_params) where key = "vwo_campaign_name") as experiment_name,
+    (select value.string_value from unnest(event_params) where key = "vwo_variation_name") as variant_name
   FROM events
-),
+)
 
-selection AS (SELECT DISTINCT
+SELECT
     event_date_1,
     user_pseudo_id_1,
     session_id_1,
-    experiment_information,
-    personalization_id,
-    personalization_name,
-    personalization_variant_id,
-    personalization_variant_name
+    experiment_id,
+    variant_id,
+    experiment_name,
+    variant_name
 FROM clean_events
-),
-
-split_experiment AS (
-    SELECT 
-        event_date_1,
-        user_pseudo_id_1,
-        session_id_1,
-        experiment_information,
-
-        -- Extracting experiment name and ID
-        SPLIT(experiment_information, '-')[OFFSET(1)] AS experiment_name,
-        SPLIT(experiment_information, '-')[OFFSET(1)] AS experiment_id,
-
-        -- Extracting variant name and ID
-        SPLIT(experiment_information, '-')[OFFSET(2)] AS variant_name,
-        SPLIT(experiment_information, '-')[OFFSET(2)] AS variant_id,
-
-        personalization_id,
-        personalization_name,
-        personalization_variant_id,
-        personalization_variant_name
-
-    FROM selection
-    WHERE session_id_1 IS NOT NULL
-)
-
-SELECT * FROM split_experiment
