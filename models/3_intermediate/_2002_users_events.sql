f0fe5a048a87b64fba43d3e8bc19330b2fd7b5ea
BigQuery Primera <analytics@datablaze.ai>
2023-11-17 12:07:08 +0000

    adjusted events

--- models/3_intermediate/_2002_users_events.sql
+++ models/3_intermediate/_2002_users_events.sql
@@ -10,22 +10,13 @@ _userinfo AS (
         event_name,
         CASE 
             WHEN lower(event_name) IN (
-                'payment_method', 'inwisselen_per_mail', 'inwisselen_nochoice', 
-                'inwisselen_beslis_ik_later', 'inwisselen_per_post', 'filter_use', 
-                'top_navigation_use', 'mobile_navigation_use', 'sort_use',
-                'cart_dropdown', 'bekijk_winkelvoorraad_button_clicked', 'tooltip_open',
-                'bekijk_winkelvoorraad_store_click', 'inwisselen_gelegenheden',
-                'tab_usage', 'bekijk_winkelvoorraad_search', 'pbl_search',
-                'stock_check', 'inwisselen_per_post', 'pbl_pageview', 'pbl_link_click',
-                'account_login', 'quick_link_use', 'promo_banner_use',
-                'phone_number_clicked', 'file_download', 'zoom_image', 'account_register',
-                'bekijk_winkelvoorraad_button_available', 'mega_menu_use',
-                'inwisselen_nochoice', 'coupon_code_added', 'pbl_file_download',
-                'top_navigation_use', 'inwisselen_per_mail', 'inwisselen_beslis_ik_later',
-                'pbl_user_navigation', 'bekijk_winkelvoorraad_contact_click'
+            'footer_social','search','scroll_to_top','formSend','user_menu','compare_products','kitsamenstelling_openen',
+            'singlePush','video_start','video_complete','footer_menu','spec_search','scroll','form_submit','refund','sign_up',
+            'form_start','chat_widget','leadgen_send','first_visit','click','combodeal_checkbox','button_click',
+            'session_start','combodeal_alternative','add_to_cart_slidein','view_search_results','file_download','quick_menu',
+            'header_click','footer_contact','video_progress','user_engagement','filters','homepage_events','click_handig_links',
+            'combodeal_alternative_slidein','kitsamenstelling_product_klik','begin_checkout','top_menu','floating_menu'
             ) THEN 'client specific events'
-            WHEN lower(event_name) LIKE 'cro%'
-            THEN 'CRO events'
             WHEN lower(event_name) IN ('add_payment_info', 'add_shipping_info', 'add_to_cart', 'add_to_wishlist', 'begin_checkout', 
                                     'purchase', 'refund', 'remove_from_cart', 'select_item', 'select_promotion', 'view_cart', 
                                     'view_item', 'view_item_list', 'view_promotion') 
