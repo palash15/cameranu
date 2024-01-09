e52708839f6646cf6215446686ed32b6a5dffe3c
BigQuery Primera <analytics@datablaze.ai>
2024-01-09 11:16:47 +0000

    adjusted max date

--- models/4_final/_9005_fact_table.sql
+++ models/4_final/_9005_fact_table.sql
@@ -1,16 +1,31 @@
 with _1000 as (
     select * from {{ref('_3000_users')}}
     WHERE EVENT_CATEGORY != 'other events' AND EXPERIMENT_NAME IS NOT NULL
+),
+
+agg as (
+    select
+        experiment_name as experiments, 
+        min(event_date) as min_date, 
+        max(event_date) as max_date,
+        string_agg(distinct variant_name, ',') as variants,
+        string_agg(distinct device, ',') as devices,
+        string_agg(distinct event_name, ',') as event_names,
+        'Cameranu' as user_role,
+        'Cameranu' as project
+    from _1000
+    group by experiment_name
 )
 
 select
-    experiment_name as experiments, 
-    min(event_date) as min_date, 
-    max(event_date) as max_date,
-    string_agg(distinct variant_name, ',') as variants,
-    string_agg(distinct device, ',') as devices,
-    string_agg(distinct event_name, ',') as event_names,
-    'Cameranu' as user_role,
-    'Cameranu' as project
-from _1000
-group by experiment_name
+    experiments,
+    min_date,
+    case when date_diff(max_date, min_date, day) > 60 then date_add(min_date, INTERVAL 30 DAY)
+         else max_date
+    end as max_date,
+    variants,
+    devices,
+    event_names,
+    user_role,
+    project
+from agg
