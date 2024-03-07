7e88d19767419aa66295f00caaa3a73770b3853f
analytics@datablaze.ai <analytics@datablaze.ai>
2024-03-07 14:49:29 +0000

    changed to latest 30 days load

--- models/3_intermediate/_4000_filter_users.sql
+++ models/3_intermediate/_4000_filter_users.sql
@@ -1,17 +1,21 @@
-with _3000 as (
-    select *, min(event_date) over (partition by experiment_name) as min_date
+with _3000_filter as (
+    select 
+        *, 
+        min(event_date) over (partition by experiment_name) as min_date,
+        dense_rank() over (partition by experiment_name order by event_date desc) as num_days
     from {{ref('_3000_users')}}
-),
-
-_3000_filter as (
-    select
-        *,
-        case when date_diff(event_date, min_date, day) > 30 then 'exclude'
-            else 'include'
-        end as filter_date
-    from _3000    
 )
+-- ,
+
+-- _3000_filter as (
+--     select
+--         *,
+--         case when date_diff(event_date, min_date, day) > 30 then 'exclude'
+--             else 'include'
+--         end as filter_date
+--     from _3000    
+-- )
 
 select *
 from _3000_filter
-where filter_date = 'include' 
+where num_days <= 30
