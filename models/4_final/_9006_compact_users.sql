0fabd4b153f5b62a3523cd4cb1b06233e5601cc2
analytics@datablaze.ai <analytics@datablaze.ai>
2024-04-23 12:01:08 +0000

    9006 modified

--- models/4_final/_9006_compact_users.sql
+++ models/4_final/_9006_compact_users.sql
@@ -8,14 +8,14 @@ with _9006 as (
     device, 
     browser,
     user_pseudo_id,
-    transaction_id,
-    revenue
+    COALESCE(transaction_id, '0') AS transaction_id,
+    COALESCE(CAST(revenue AS STRING), '0') AS revenue,
     FROM {{ ref('_9001_device_data') }}
 )
  
 SELECT distinct event_date, experiment_name,variant_name, user_type,channel_grouping_session, device, browser,
-                    string_agg(user_pseudo_id) as user_pseudo_ids,
-                    string_agg(ifnull(transaction_id,'0')) as transaction_ids,
-                    string_agg(ifnull(cast(revenue as string),'0')) as revenues
+                STRING_AGG(user_pseudo_id, ',') AS user_pseudo_ids,
+                STRING_AGG(transaction_id, ',') AS transaction_ids,
+                STRING_AGG(revenue, ',') AS revenues
 FROM _9006
 GROUP BY event_date, experiment_name, variant_name, device, user_type, channel_grouping_session, browser            
